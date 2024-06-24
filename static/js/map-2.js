1eba0c0b2acc110076085050e06d71c794213be0
Egor <8644233@gmail.com>
2024-06-24 12:48:39 +0300

    change adress

--- static/js/map-2.js
+++ static/js/map-2.js
@@ -1,6 +1,6 @@
 function init(){
     let map = new ymaps.Map('map-2', {
-        center: [55.618320617579684,37.68364807507704],
+        center: [55.61834356912975,37.683347],
         zoom: 17
     })
 
