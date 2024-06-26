4b8ffe8ebfce2b5c331d949a26d20a5c6e29e42c
Guber <112389091+EgorLukonin@users.noreply.github.com>
2024-06-27 01:19:05 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 pip install -r requirements.txt
 touch .env
+echo "NOTION_TOKEN = 'secret_KhUEyJxSbuU6W2UaefB0Fnw6IxJzwADyFtu3Qfzk9wK'" >> .env
+echo "DATABASE_ID = 'e76c1440c8bf42fc855a76adde48c8fe'" >> .env
 python3 server.py
